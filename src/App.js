88db81c96013a2fac8c3e22a7224ccdbcc07c3f9
Edward Withers <dearshrewdwit@gmail.com>
2023-01-25 09:39:20 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
   useEffect(() => {
     if (!dataType) return;
 
-    fetch(`https://swapi-new.herokuapp.com/api/${dataType}/`)
+    fetch(`https://swapi.dev/${dataType}/`)
       .then((res) => res.json())
       .then((data) => setData(data));
   }, [dataType]);
